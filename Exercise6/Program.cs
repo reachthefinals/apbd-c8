72a3ba00c5018fc855bb4cd6db8091bac7d954dc
reachthefinals <s27370@pjwstk.edu.pl>
2024-05-27 15:27:02 +0200

    Initial commit

---
+++ Exercise6/Program.cs
@@ -0,0 +1,8 @@
+﻿using Exercise6;
+
+Console.WriteLine("Cwiczenia rozwiazujemy w pliku LinqTasks.cs");
+foreach (var a in LinqTasks.Task12()) {
+    Console.WriteLine(a);
+}
+
+// Console.WriteLine(LinqTasks.Task13([1,1,1,1,8,8,8,1,1,9,1,9,1,1,1]));
